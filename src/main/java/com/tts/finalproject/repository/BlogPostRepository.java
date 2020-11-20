72b465f963b612787e39b5a40913db0cf8ad0b55
Samber9891 <Slem0119@gmail.com>
2020-11-20 00:59:41 -0500

    no change

--- src/main/java/com/tts/finalproject/repository/BlogPostRepository.java
+++ src/main/java/com/tts/finalproject/repository/BlogPostRepository.java
@@ -1,11 +1,13 @@
 package com.tts.finalproject.repository;
 
+
 import com.tts.finalproject.model.BlogPost;
+
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-    
+  
 }
