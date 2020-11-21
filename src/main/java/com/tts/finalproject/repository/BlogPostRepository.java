002dee9015812cda0e8bb371464ecbad8f013320
Samber9891 <Slem0119@gmail.com>
2020-11-21 17:41:02 -0500

    added find methods

--- src/main/java/com/tts/finalproject/repository/BlogPostRepository.java
+++ src/main/java/com/tts/finalproject/repository/BlogPostRepository.java
@@ -10,12 +10,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-  
 
-    List<BlogPost> findById(long id); 
 
+    List<BlogPost> findAll();
+
+    BlogPost findById(long id);
     List<BlogPost> findByGenre(String genre);
 
     @Query("SELECT DISTINCT blogPost.genre FROM BlogPost blogPost")
     List<String> findDistinctGenres();
+	
 }
