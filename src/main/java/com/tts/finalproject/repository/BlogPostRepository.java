ed8a173efc447baf69fc60715fac87db15c144f2
Samber9891 <Slem0119@gmail.com>
2020-11-21 14:26:27 -0500

    added findBy and query

--- src/main/java/com/tts/finalproject/repository/BlogPostRepository.java
+++ src/main/java/com/tts/finalproject/repository/BlogPostRepository.java
@@ -1,13 +1,21 @@
 package com.tts.finalproject.repository;
 
+import java.util.List;
 
 import com.tts.finalproject.model.BlogPost;
 
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
   
+
+    List<BlogPost> findById(long id); 
+
+    List<BlogPost> findByGenre(String genre);
+
+    @Query("SELECT DISTINCT blogPost.genre FROM BlogPost blogPost")
+    List<String> findDistinctGenres();
 }
