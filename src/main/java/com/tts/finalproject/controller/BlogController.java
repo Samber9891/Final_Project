a09ec0411aab9643f66f3019b5f7918a67fc940b
Samber9891 <Slem0119@gmail.com>
2020-11-21 17:41:20 -0500

    added filter methods

--- src/main/java/com/tts/finalproject/controller/BlogController.java
+++ src/main/java/com/tts/finalproject/controller/BlogController.java
@@ -6,24 +6,41 @@ import java.util.Optional;
 
 import com.tts.finalproject.model.BlogPost;
 import com.tts.finalproject.repository.BlogPostRepository;
+import com.tts.finalproject.service.BlogPostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class BlogController {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
-
+    
+    @Autowired
+    BlogPostService blogPostService; 
      
     public List<BlogPost> posts = new ArrayList<>();
+
+    @ModelAttribute("genres")
+    public List<String> genres() {
+        return blogPostService.findDistinctGenres(); 
+    }
+
+    @GetMapping("/filter")
+    public String filter(@RequestParam(required = false) String genre, Model model) {
+        List<BlogPost> filtered = blogPostService.findByGenre(genre);
+        model.addAttribute("posts", filtered); // Overrides the @ModelAttribute above.
+        return "index";
+    }
 
     @GetMapping(value="/")
     public String index(BlogPost blogPost, Model model) {
@@ -42,9 +59,10 @@ public class BlogController {
 
     @GetMapping(value = "/fullblog/{id}")
     public String showBlog(@PathVariable Long id, Model model) {
-        Optional<BlogPost> blogPost =  blogPostRepository.findById(id);
-        model.addAttribute(blogPost); 
-        return "fullblog";
+        Optional<BlogPost> posts =  blogPostRepository.findById(id);
+        model.addAttribute("posts", posts);
+        System.out.println(posts);
+        return "redirect:/fullblog/" + blogPost.getId();
     }
       // end of create new blog
 
